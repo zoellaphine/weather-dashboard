1eb88633353abc54f558e756863db078dffc0df3
katzeundmais <zoellaphine@gmail.com>
2024-06-13 21:11:24 -0400

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Description
+
+This application is a very simple dashboard that can display the weather for any general city. 
+The project was created to gain experience connecting an API into a codebase and navigating the results of those calls.
+
+Navigating the API data ended up being very tricky for me. Because it's many nested arrays and I wasn't fully sure of the syntax for finding specific items within the arrays I had to spend a long time researching how to get to individual pieces of the array. Along with this, my program unfortunately only is a 4-day forecast, rather than a 5-day. I don't know why, but whenever I tried to populate the content of the final day it just wouldn't work. The array of divs seemed to be iterating properly but everything kept conflicting and throwing errors.
+
+## Usage
+
+Opening the GitHub pages link or running the program locally will present the generic homepage. The user can either enter a city into the search bar and click the search button, or there are a few pre-made buttons with popular cities which can be used to find a city without typing. A 4-day forecast should be displayed including today's weather in a large box and 3 smaller boxes with the next 3 days.
---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+// API key and url
+const apiKey = 'd96876d02cf45f7fa9bfb328eb5b12f2';
+const forecastApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
+const geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+
+const searchBar = document.querySelector('.searchBar');
+const forecastDays = document.querySelectorAll('.week');
+
+async function getLatLong(city) {
+    let query = geoApiUrl + city + '&limit=5' + `&appid=${apiKey}`;
+    //fetch the latitude and longitude for the given city name
+    const response = await fetch(geoApiUrl + city + '&limit=5' + `&appid=${apiKey}`);
+
+    // display an error message if status is 404, else call getForecast with
+    // fetched latitude and longitude
+    if (response.status == 404) {
+
+    } else {
+        let data = await response.json();
+
+        let lat = data[0].lat;
+        let long = data[0].lon;
+        document.querySelector('.city').innerHTML = city;
+        getForecast(lat, long, city);
+    }
+}
+
+async function getForecast(lat, long, city) {
+    const response = await fetch(forecastApiUrl + 'lat=' + lat + '&lon=' + long + `&appid=${apiKey}` + '&units=imperial');
+
+    if (response.status == 404) {
+
+    } else {
+        let data = await response.json();
+        let days = [];
+        // for (let i = 0; i < 6; i++) {
+        //     days.push(data.list[i]);
+        // }
+        days.push(data.list[4]);
+        days.push(data.list[12])
+        days.push(data.list[20])
+        days.push(data.list[28])
+        days.push(data.list[36])
+        console.log(days);
+
+        // Set the data for the first day card
+        document.querySelector('.today').style.display = "block";
+        document.querySelector('.date').innerHTML = '(' + days[0].dt_txt + ')';
+        document.querySelector('.temp').innerHTML = 'Temp: ' + days[0].main.temp + '°F';
+        document.querySelector('.wind').innerHTML = 'Wind: ' + days[0].wind.speed + 'MPH';
+        document.querySelector('.humidity').innerHTML = 'Humidity: ' + days[0].main.humidity + '%';
+
+        // Set the data for each day in the forecast
+        let k=0;
+        for (let j = 1; j <= 4; j++) {
+            const day = document.querySelector('.week').children[k];
+            day.style.display = "block";
+            day.querySelector('.date').innerHTML = '(' + days[j].dt_txt + ')';
+            day.querySelector('.temp').innerHTML = 'Temp: ' + days[j].main.temp + '°F';
+            day.querySelector('.wind').innerHTML = 'Wind: ' + days[j].wind.speed + 'MPH';
+            day.querySelector('.humidity').innerHTML = 'Humidity: ' + days[j].main.humidity + '%';
+            k += 1;
+        }
+    }
+}
+
+document.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains('searchButton')) {
+        getLatLong(searchBar.value);
+    } else if (event.target.classList.contains('preset')) {
+        getLatLong(event.target.innerText);
+    }
+})
